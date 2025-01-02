86c2630a234988873838bfa578740846e89abe4a
YuvalNuri <yuvalnuri57@gmail.com>
2025-01-02 16:25:30 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FCRegister from './FCComps - tamir/FCRegister'
-import FCLogIn from './FCComps - tamir/FCLogIn'
-import FCProfile from './FCComps - tamir/FCProfile'
+import FCRegister from './FCComps/FCRegister'
+import FCLogIn from './FCComps/FCLogIn'
+import FCProfile from './FCComps/FCProfile'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
- 
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = () => {
+    if (localStorage.getItem("users") != undefined) {
+      setUsers(JSON.parse(localStorage.getItem("users")));
+    }
+    else {
+      setUsers([]);
+    }
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
   return (
-    <>
-{/*<FCRegister/>
-<FCLogIn/>*/}
-<FCProfile/>
-
-    </>
+    <div>
+    <Routes>
+      <Route path="/register" element={<FCRegister/>}/>
+      <Route path="/register" element={<FCProfile/>}/>
+      <Route path="/register" element={<FCLogIn/>}/>
+    </Routes>
+    </div>
   )
 }
 
